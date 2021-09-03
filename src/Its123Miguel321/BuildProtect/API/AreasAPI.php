44c51f3b83b830af6408786380d1ab106aefd818
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-02 19:14:21 -0700

    Update AreasAPI.php

--- src/Its123Miguel321/BuildProtect/API/AreasAPI.php
+++ src/Its123Miguel321/BuildProtect/API/AreasAPI.php
@@ -3,11 +3,22 @@
 namespace Its123Miguel321\BuildProtect\API;
 
 use Its123Miguel321\BuildProtect\Area;
+use Its123Miguel321\BuildProtect\BuildProtect;
 
 class AreasAPI
 {
 	/** @var BuildProtect $main */
 	public $main;
 	/** @var EventListener $events */
-	publci $events
+	public $events
+	
+	/**
+	 * @param BuildProtect $main
+	 */
+	public function __construct(BuildProtect $main)
+	{
+		$this->main = $main;
+	}
+	
+		
 }

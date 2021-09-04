7a2e79da3b126ac05d14c0d3132523e29c0c8cb2
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-04 12:24:36 -0700

    Update YamlProvider.php

--- src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
+++ src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
@@ -3,6 +3,7 @@
 namespace Its123Miguel321\BuildProtect\provider;
 
 use Its123Miguel321\BuildProtect\BuildProtect;
+use Its123Miguel321\BuildProtect\provider\DataProvider;
 
 use pocketmine\utils\Config;
 

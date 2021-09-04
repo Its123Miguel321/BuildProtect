5ad4dc77cfbd84b790be2e146e1744a0de38b281
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-04 12:51:51 -0700

    Update YamlProvider.php

--- src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
+++ src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
@@ -9,5 +9,37 @@ use pocketmine\utils\Config;
 
 class YamlProvider extends DataProvider
 {
-
+	/** @var BuildProtect $plugin */
+	public $plugin;
+	/** @var Config $yaml */
+	public $yaml
+	
+	
+	/**
+	 * YamlProvider constructer.
+	 * 
+	 * @param BuildProtect $plugin;
+	 *
+	 */
+	public function __construct(BuildProtect $plugin)
+	{
+		$this->plugin = $plugin;
+		
+		$this->open();
+	}
+	
+	
+	
+	/**
+	 * Checks if Area name already exists
+	 * 
+	 * @param string $area
+	 * 
+	 */
+	public function areaExists(string $area) : bool
+	{
+		$areas = $this->yaml->get("areas", []);
+		
+		return isset(array_keys($areas, ["Name" => $name], true));
+	}
 }

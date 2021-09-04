387f960051b8d75d1ffecebfbe40bb1ff5e237c7
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-04 15:50:34 -0700

    Update YamlProvider.php

--- src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
+++ src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
@@ -32,7 +32,7 @@ class YamlProvider extends DataProvider
 	
 	
 	/**
-	 * Checks if Area name already exists
+	 * Checks if Area name already exists.
 	 * 
 	 * @param Area $area
 	 * 
@@ -42,6 +42,38 @@ class YamlProvider extends DataProvider
 		$areas = $this->yaml->get("areas", []);
 		
 		return isset($areas[$area->getId()][$areas->getName()]);
+	}
+	
+	/**
+	 * Saves an area.
+	 * 
+	 * @param Area $area
+	 * 
+	 */
+	public function saveArea(Area $area) : void
+	{
+		$areas = $this->yaml->get("areas", []);
+		
+		$areas[$area->getId()] = array("Name" => $area->getName(), "Creator" => $area->getCreator(), "Pos1" => $area->getPos1(), "Pos2" => $area->getPos2, "Commands" => $area->getCommands(), "Permissions" => $area->getPermissions(), "BlockBreaking" => $area->getSetting("Breaking"), "BlockPlacing" => $area->getSetting("Placing"), "PvP" => $area->getSetting("PvP"), "Flight" => $area->getSetting("Flight"));
+		
+		$this->yaml->set("areas", $areas);
+		$this->save();
+	}
+	
+	/**
+	 * Deletes an area.
+	 * 
+	 * @param Area $area
+	 * 
+	 */
+	public function deleteArea(Area $area) : void
+	{
+		$areas = $this->yaml->get("areas", []);
+		
+		unset($areas[$area->getId()]);
+		
+		$this->yaml->set("areas", $areas);
+		$this->save();
 	}
 	
 	public function open() : void

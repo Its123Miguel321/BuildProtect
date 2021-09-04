d66c0bc256a0218172f55f0deed74ee95d52d0ec
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-04 12:23:54 -0700

    Create YamlProvider.php

---
+++ src/Its123Miguel321/BuildProtect/provider/YamlProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Its123Miguel321\BuildProtect\provider;
+
+use Its123Miguel321\BuildProtect\BuildProtect;
+
+use pocketmine\utils\Config;
+
+class YamlProvider extends DataProvider
+{
+
+}

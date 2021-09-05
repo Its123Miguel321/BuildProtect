20dbecadbfa489122d4783cff7d3b29a06d66d3f
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-05 11:38:09 -0700

    Update AreasAPI.php
    New AreasAPI
    Added Commands and Permissions with this new API

--- src/Its123Miguel321/BuildProtect/API/AreasAPI.php
+++ src/Its123Miguel321/BuildProtect/API/AreasAPI.php
@@ -4,7 +4,9 @@ namespace Its123Miguel321\BuildProtect\API;
 
 use Its123Miguel321\BuildProtect\BuildProtect;
 
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\level\Position;
+use pocketmine\Player;
 
 class AreasAPI
 {
@@ -64,6 +66,117 @@ class AreasAPI
 		$id = $this->getMain()->getProvider()->countAreas() + 1;
 		
 		$this->getMain()->getProvider()->saveArea(new Area($id, $name, $creator, $pos1, $pos2, $commands, $permissons, $breaking, $placing, $pvp, $flight));
+	}
+	
+	
+	
+	/**
+	 * Deletes an area from its name
+	 * 
+	 * @param string $name
+	 * 
+	 */
+	public function deleteArea(string $name) : void
+	{
+		$id = $this->getMain()->getProvider()->getAreaId($name);
+		$area = $this->getMain()->getProvider()->getArea($id);
+		
+		$this->getMain()->getProvider()->deleteArea($area);
+	}
+	
+	
+	
+	/**
+	 * Checks if player or block is inside an area.
+	 * 
+	 * @param Position $pos
+	 * 
+	 * @return bool
+	 * 
+	 */
+	public function isInside(Position $pos) : bool
+	{
+		foreach($this->getMain()->getProvider()->getAreas() as $areas)
+		{
+			$area = $this->getMain()->getProvider()->getArea(array_values($areas));
+			$pos1 = $area->getPos1();
+			$pos2 = $area->getPos2();
+			$x = array_flip(range($pos1[0], $pos2[0]));
+			$y = array_flip(range($pos1[1], $pos2[1]));
+			$z = array_flip(range($pos1[2], $pos2[2]));
+			$level = $this->getMain()->getProvider()->getAreaLevel($area);
+			if($pos->getLevel() === $level)
+			{
+				if(isset($x[$pos->getX()]))
+				{
+					if(isset($y[$pos->getY()]))
+					{
+						if(isset($z[$pos->getZ()]))
+						{
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+	
+	
+	
+	/**
+	 * Runs all area commands once a player goes into one.
+	 * 
+	 * @param string $areaName
+	 * @param Player $player
+	 * 
+	 */
+	public function runAreaCommands(string $areaName, Player $player) : void
+	{
+		$id = $this->getMain()->getProvider()->getAreaId($areaName);
+		$area = $this->getMain()->getProvider()->getArea($id);
+		
+		foreach($area->getCommands() as $command)
+		{
+			if(str_contains($command, "rca ")) {
+				$this->getMain()->getServer()->dispatchCommand($player, str_replace("{player}", $player->getName(), $command));
+			} else {
+				$this->getMain()->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $player->getName(), $command));
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Checks if a player has a permission in an area they go into.
+	 * 
+	 * @param string $name
+	 * 
+	 */
+	public function playerHasAreaPermissons(Player $player, string $areaName) : bool
+	{
+		$id = $this->getMain()->getProvider()->getAreaId($areaName);
+		$area = $this->getMain()->getProvider()->getArea($id);
+		
+		foreach($this->getMain()->getProvider()->getAreaPermissions() as $permission)
+		{
+			if($player->hasPermission($permisson))
+			{
+				return true;
+			}
+		}
+	}
+	
+	public function getSelections(Player $player) : array
+	{
+		return $this->getMain()->getEventListener()->selections[$player->getName()];
+	}
+	
+	public function hasSelection(Player $player, string $selection) : bool
+	{
+		return isset($this->getMain()->getEventListener()->selections[$player->getName()][$selection]);
 	}
 	
 	/**

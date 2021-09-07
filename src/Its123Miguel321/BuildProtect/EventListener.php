7b8e151b16df6f7f947237cf3b2c6fded9bb1409
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2021-09-07 00:03:21 -0700

    Update EventListener.php

--- src/Its123Miguel321/BuildProtect/EventListener.php
+++ src/Its123Miguel321/BuildProtect/EventListener.php
@@ -12,137 +12,192 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\level\Position;
 use pocketmine\Player;
+use pocketmine\plugin\Plugin;
 
 class EventListener implements Listener{
 
-	private $plugin;
-	
+	/** @var BuildProtect $main */
+	public $main;
+	/** @var array $wandClicks */
 	private $wandClicks = [];
-	
+	/** @var array $selections */
 	public static $selections = [];
 	
-	public function __construct(BuildProtect $plugin){
+	
+	
+	/**
+	 * EventListener constructer.
+	 *
+	 * @param BuildProtect $main
+	 * 
+	 */
+	public function __construct(BuildProtect $main)
+	{
 		$this->plugin = $plugin;
 	}
 	
-	public function onAttack(EntityDamageByEntityEvent $event){
-	    
-	    $victim = $event->getEntity();
-	    $attacker = $event->getDamager();
-	    
-	    if($event->isCancelled()){
-	        return;
-	    }
-	    
-	    if(!($victim instanceof Player && $attacker instanceof Player)){
-	        return;
-	    }
-	    
-	    if($attacker->hasPermission("buildprotect.bypass")){
-                return;
-             }
-	    
-	    if(!$this->plugin->isInside(new Position($attacker->getX(), $attacker->getY(), $attacker->getZ(), $attacker->getLevel()))){
-	        return;
-	    }
-	    
-	    if(!$this->plugin->isInside(new Position($victim->getX(), $victim->getY(), $victim->getZ(), $victim->getLevel()))){
-	        return;
-	    }
-	    
-	    foreach($this->plugin->builds->get("builds", []) as $areas){
-	        if($areas["PvP"] == false){
-			    if($this->plugin->config->get("AreaMessages") == true){
-	                $attacker->sendMessage($this->plugin->config->get("AttackingkDisabled"));
-			    }
-	            $event->setCancelled();
-	        }
-	    }
-	    
-	}
 	
-	public function onBreak(BlockBreakEvent $event){
-	    
-	    $player = $event->getPlayer();
-	    $block = $event->getBlock();
-	    
-	    if($event->isCancelled()){
-	        return;
-	    }
-	    
-	    if($player->hasPermission("buildprotect.bypass")){
-            return;
-        }
-	    
-	    if(!$this->plugin->isInside(new Position($block->getX(), $block->getY(), $block->getZ(), $block->getLevel()))){
-	        return;
-	    }
-	    
-	    foreach($this->plugin->builds->get("builds", []) as $areas){
-	        if($areas["BlockBreaking"] == false){
-	            if($this->plugin->config->get("AreaMessages") == true){
-	            	$player->sendMessage($this->plugin->config->get("BreakingDisabled"));
+	
+	/**
+	 * Checks if a player is getting hit in an area where pvp is disabled.
+	 *
+	 * @param EntityDamageEventByEntity $event
+	 * 
+	 */
+	public function onAttack(EntityDamageByEntityEvent $event)
+	{
+		$victim = $event->getEntity();
+		$attacker = $event->getDamager();
+		
+		if($this->getMain()->getApi()->isInside($attacker->asPosition()) || $this->getMain()->getApi()->isInside($victim->asPosition()))
+		{
+			foreach($this->getMain()->getProvider()->getAreas() as $area)
+			{
+				$areaName = $area["Name"];
+				
+				if($attacker->hasPermission("buildprotect.bypass") || $attacker->hasPermission("buildprotect.admin") || $this->getMain()->getApi()->playerHasAreaPermissions($attacker, $areaName)))
+				{
+					return;
 				}
-	            $event->setCancelled();
-	        }
-	    }
-	}
-	
-	public function onPlace(BlockPlaceEvent $event){
-	    
-	    $player = $event->getPlayer();
-	    $block = $event->getBlock();
-	    
-	    if($event->isCancelled()){
-	        return;
-	    }
-	    
-	    if($player->hasPermission("buildprotect.bypass")){
-            	return;
-            }
-	    
-	    if(!$this->plugin->isInside(new Position($block->getX(), $block->getY(), $block->getZ(), $block->getLevel()))){
-	        return;
-	    }
-	    
-	    foreach($this->plugin->builds->get("builds", []) as $areas){
-	        if($areas["BlockPlacing"] == false){
-	            if($this->plugin->config->get("AreaMessages") == true){
-	            	$player->sendMessage($this->plugin->config->get("PlacingDisabled"));
-				}
-	            $event->setCancelled();
-	        }
-	    }
-	}
-	
-	public function onMove(PlayerMoveEvent $event){
-	    
-	    $player = $event->getPlayer();
-	    
-	    if($event->isCancelled()){
-	        return;
-	    }
-	    
-        if($player->hasPermission("buildprotect.bypass")){
-            return;
-        }
-	    
-	    if(!$this->plugin->isInside(new Position($player->getX(), $player->getY(), $player->getZ(), $player->getLevel()))){
-	        return;
-	    }
-	    
-	    foreach($this->plugin->builds->get("builds", []) as $areas){
-	        if($areas["Flight"] == false){
-	            if($player->isFlying()){
-	                if($this->plugin->config->get("AreaMessages") == true){
-	            		$player->sendMessage($this->plugin->config->get("FlyingDisabled"));
+				
+				if($areas["PvP"] == false)
+				{
+	            	if($this->getMain()->getConfig()->get("AreaMessages"))
+					{
+	            		$player->sendMessage($this->getMain()->getConfig()->get("AttackingDisabled"));
 					}
-	                $player->setAllowFlight(false);
-	                $player->setFlying(false);
-	                return;
-	            }
-	        }
-	    }
+						
+					$event->setCancelled();
+	        	}
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Checks if a player is breaking a block in an area where it is disabled.
+	 * 
+	 * @param BlockBreakEvent $event
+	 * 
+	 */
+	public function onBreak(BlockBreakEvent $event)
+	{
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		
+		if($event->isCancelled())
+		{
+			return;
+		}
+		
+		if($this->getMain()->getApi()->isInside($block->asPosition()))
+		{
+			foreach($this->getMain()->getProvider()->getAreas() as $area)
+			{
+				$areaName = $area["Name"];
+				
+				if($player->hasPermission("buildprotect.bypass") || $player->hasPermission("buildprotect.admin") || $this->getMain()->getApi()->playerHasAreaPermissions($player, $areaName)))
+				{
+					return;
+				}
+				
+				if($areas["BlockBreaking"] == false)
+				{
+	            	if($this->getMain()->getConfig()->get("AreaMessages"))
+					{
+	            		$player->sendMessage($this->getMain()->getConfig()->get("BreakingDisabled"));
+					}
+						
+					$event->setCancelled();
+	        	}
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Checks if a player is placing a block in an area where it is disabled.
+	 * 
+	 * @param BlockPlaceEvent $event
+	 * 
+	 */
+	public function onPlace(BlockPlaceEvent $event)
+	{
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		
+		if($event->isCancelled())
+		{
+			return;
+		}
+		
+		if($this->getMain()->getApi()->isInside($block->asPosition()))
+		{
+			foreach($this->getMain()->getProvider()->getAreas() as $area)
+			{
+				$areaName = $area["Name"];
+				
+				if($player->hasPermission("buildprotect.bypass") || $player->hasPermission("buildprotect.admin") || $this->getMain()->getApi()->playerHasAreaPermissions($player, $areaName)))
+				{
+					return;
+				}
+				
+				if($area["BlockPlacing"] == false)
+				{
+					if($this->getMain()->getConfig()->get("AreaMessages"))
+					{
+						$player->sendMessage($this->getMain()->getConfig()->get("PlacingDisabled"));
+					}
+				
+					$event->setCancelled();
+				}
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Checks if a player has entered an area
+	 * 
+	 * @param PlayerMoveEvent $event
+	 * 
+	 */
+	public function onMove(PlayerMoveEvent $event)
+	{
+		$player = $event->getPlayer();
+		
+		if($event->isCancelled())
+		{
+			return;
+		}
+		
+		if($this->getMain()->getApi()->isInside($player->asPosition()))
+		{
+			foreach($this->getMain()->getProvider()->getAreas() as $area)
+			{
+				$areaName = $area["Name"];
+				
+				if($player->hasPermission("buildprotect.bypass") || $player->hasPermission("buildprotect.admin") || $this->getMain()->getApi()->playerHasAreaPermissions($player, $areaName)))
+				{
+					return;
+				}
+				
+				// Note to self: fix this so it only runs once when the player enters the area!
+				$this->getMain()->getApi()->runAreaCommands($areaName, $player);
+				
+				if($area["Flight"] == true)
+				{
+					if(!($player->getGamemode() == 0))
+					{
+						$player->setAllowFlight(false);
+						$player->setFlying(false);
+					}
+				}
+			}
+		}
 	}
 	
 	public function onPos1(BlockBreakEvent $event){
@@ -211,5 +266,10 @@ class EventListener implements Listener{
 		self::$selections[$player->getName()]["pos2"] = ["x" => $x, "y" => $y, "z" => $z, "level" => $level];
 		
 		$player->sendMessage("§l§a(!) §r§7Selected second position at §6" . $x . "§7, §6" . $y . "§7, §6" . $z . " §7in §6" . $level . "§7!");
+	}
+		 
+	public function getMain() : Plugin
+	{
+		return $this->main;
 	}
 }
